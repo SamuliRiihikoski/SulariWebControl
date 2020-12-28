4b876a54d721551ff6192e5a167201d933d1387a
SamuliRiihikoski <haikalle@gmail.com>
2020-12-28 19:54:40 +0200

    added commandBuffer. Now it dosen't miss any commands. a bit slow thou

--- FrontBackEnd/server/index.js
+++ FrontBackEnd/server/index.js
@@ -4,36 +4,57 @@ var path = require('path');
 var io = require('socket.io')(server);
 var net = require('net');
 
-let sensor = "";
+let commandBuffer = [];
+let arduinoReady = true;
+let updateTime = 1000;
+let index = 0;
 
 server.listen(80);
 
-app.get('/', function(request, response) {
+app.get('/', function(request, response) 
+{
     response.sendFile(path.join(__dirname + '/public/index.html'));
-    console.log("Moikka");
-
+    console.log("New connection!");
 });
 
-app.get('/update', function(request, response) {
+app.get('/update', function(request, response) 
+{
     console.log("Viesti:" + request.query.sensor);
     let num = parseInt(request.query.sensor);
     num = num / 100;
     let text = num.toString();
     io.emit('sensor', text);
+
+    arduinoReady = true;
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function(socket) 
+{
     socket.on('message', function(message) {
-        sensor = "LAMP";
-        io.emit('message', message);
+
+        if (arduinoReady)
+            io.emit('message', message);
+        else
+            commandBuffer.push(message);
     });
 });
 
-setInterval(function(){  // Commands needs to have : in the end. Reason because in arduino that is the mark for end of the command
-    if (sensor == "LAMP") {
-        io.emit('message', 'LAMP:'); 
-        sensor = "";
+setInterval(function() {  // Commands needs to have : in the end. Reason because in arduino that is the mark for end of the command
+    
+    if (commandBuffer.length > 0)
+    {
+        sendCommand = commandBuffer.pop();
+        io.emit('message', sendCommand);
+        index = 0;
     }
-    else
-        io.emit('message', 'TEMP:');  
-}, 5000);
+    
+    if (index == 5)
+    {
+        arduinoReady = false;
+        io.emit('message', 'TEMP:');
+        index = 0;
+    }
+
+    index++;
+    
+}, 2000);
