624c0ca8014708d7181e177eccfa06c8740b91dd
SamuliRiihikoski <haikalle@gmail.com>
2020-12-29 14:47:10 +0200

    communication works much better now!

--- FrontBackEnd/server/index.js
+++ FrontBackEnd/server/index.js
@@ -4,9 +4,6 @@ var path = require('path');
 var io = require('socket.io')(server);
 var net = require('net');
 
-let commandBuffer = [];
-let arduinoReady = true;
-let updateTime = 1000;
 let index = 0;
 
 server.listen(80);
@@ -25,27 +22,17 @@ app.get('/update', function(request, response)
     let text = num.toString();
     io.emit('sensor', text);
 
-    arduinoReady = true;
 });
 
 io.on('connection', function(socket) 
 {
     socket.on('message', function(message) {
-        commandBuffer.push(message);
+        io.emit('message', message);
         index = 0;
     });
 });
 
 setInterval(function() {  // Commands needs to have : in the end. Reason because in arduino that is the mark for end of the command
-    
-    if (arduinoReady == false) return;
-
-    if (commandBuffer.length > 0 && index > 0)
-    {
-        sendCommand = commandBuffer.pop();
-        io.emit('message', sendCommand);
-        index = 0;
-    }
     
     if (index == 5)
     {
